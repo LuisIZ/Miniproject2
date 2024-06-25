3bf6d13e1f1584d7aedd71cbbdbee66a5d2b0489
LuisIZ <luis.izaguirre@utec.edu.pe>
2024-06-24 21:12:57 -0500

    Correcting error of black formatter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Miniproject 2'
-copyright = '2024, Luis Izaguirre'
-author = 'Luis Izaguirre'
-release = '24-06-2024'
+project = "Miniproject 2"
+copyright = "2024, Luis Izaguirre"
+author = "Luis Izaguirre"
+release = "24-06-2024"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
